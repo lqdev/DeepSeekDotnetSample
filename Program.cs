bd0f39f25b8fb0091062dbeeb654a99afcf6add7
lqdev <lqdev@outlook.com>
2025-01-29 19:41:23 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Azure;
+using Azure.AI.Inference;
+using Microsoft.Extensions.AI;
+
+IChatClient chatClient = 
+    new ChatCompletionsClient(
+        new Uri("https://models.inference.ai.azure.com"), 
+        new AzureKeyCredential(Environment.GetEnvironmentVariable("GITHUB_TOKEN")))
+        .AsChatClient("DeepSeek-R1");
+
+var streamingResponse = chatClient.CompleteStreamingAsync("What appears once in a minute, twice in a moment, but never in a thousand years?");
+
+await foreach (var response in streamingResponse)
+{
+    Console.Write(response.Text);
+}
